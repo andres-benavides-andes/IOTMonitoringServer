fdcd141b6046ac0315c84c8b658e1d642cbc6dcb
Andres Benavides <f.benavidesv@uniandes.edu.co>
2022-07-10 19:39:59 -0500

    Ajsute variable de conexion mqtt

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "44.200.118.160"
+MQTT_HOST = "3.236.134.79"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
