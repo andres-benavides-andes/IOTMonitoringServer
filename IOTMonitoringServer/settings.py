5e5bb4155fd79f8ec8f6c95bd474db9b310a73d4
Andres Benavides <f.benavidesv@uniandes.edu.co>
2022-07-15 16:20:53 -0500

    ajustes de ips

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.228.9.196",  # Dirección IP de la base de datos
+        "HOST": "44.201.30.186",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.236.134.79"
+MQTT_HOST = "3.234.224.124"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
