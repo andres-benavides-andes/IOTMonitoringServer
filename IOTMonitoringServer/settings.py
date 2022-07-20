17427a870354d7a31c9500751a82eb10d1b50df3
Andres Benavides <f.benavidesv@uniandes.edu.co>
2022-07-20 12:25:49 -0500

    cambios ip

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.237.240.98"]
+ALLOWED_HOSTS = ["localhost", "44.193.77.139"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.226.237.20",  # Dirección IP de la base de datos
+        "HOST": "3.91.34.134",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.235.152.166"
+MQTT_HOST = "44.204.144.253"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
